9a3c43d2dfb6717ad10fc9c9b577e51685930907
何永强 <heyongqiang@heyongqangdeMBP.lan>
2017-07-02 17:03:31 +0800

    update

---
+++ src/components/Header.vue
@@ -0,0 +1,72 @@
+<template>
+   <div class="app-head">
+      <div class="app-head-inner">
+        <img src="../assets/logo.png">
+        <div class="head-nav">
+          <ul class="nav-list">
+            <li>登录</li>
+            <li class="nav-pile">|</li>
+            <li >注册</li>
+            <li class="nav-pile">|</li>
+            <li >关于</li>
+          </ul>
+        </div>  
+      </div>
+    </div>
+
+</template>
+
+<script>
+export default {
+  name: 'Header',
+  data () {
+    return {
+      msg: 'this is a Header component'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.app-head {
+  background: #363636;
+  color: #b2b2b2;
+  height: 90px;
+  line-height: 90px;
+  width: 100%;
+}
+.app-head-inner {
+  width: 1200px;
+  margin: 0 auto;
+}
+.head-logo {
+  float: left;
+  margin-top: 20px;
+}
+.head-logo img {
+  width: 50px;
+}
+.head-nav {
+  float: right;
+}
+.head-nav ul {
+  overflow: hidden;
+}
+.head-nav li {
+  cursor: pointer;
+  float: left;
+}
+.nav-pile {
+  padding: 0 10px;
+}
+.app-foot {
+  text-align: center;
+  height: 80px;
+  width: 100%;
+  line-height: 80px;
+  background: #e3e4e8;
+  clear: both;
+  margin-top: 30px;
+}
+</style>
